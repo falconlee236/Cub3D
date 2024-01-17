ae7cd6c306aab8729a5c9b377d506501f7da881f
falconlee236 <falconlee236@cau.ac.kr>
2024-01-17 15:59:53 +0900

    Update pcub3d add sturct in header file

--- cub3d.h
+++ cub3d.h
@@ -6,7 +6,7 @@
 /*   By: sangylee <sangylee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 17:23:36 by sangylee          #+#    #+#             */
-/*   Updated: 2024/01/17 15:42:28 by sangylee         ###   ########.fr       */
+/*   Updated: 2024/01/17 15:59:27 by sangylee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,4 +25,39 @@ typedef struct s_vec{
 	double	y;
 }	t_vec;
 
+typedef struct s_img{
+	int				w;
+	int				h;
+	int				bits_per_pixel;
+	int				size_line;
+	int				endian;
+	void			*ptr;
+	unsigned int	*addr;
+}	t_img;
+
+typedef struct s_pixel{
+	double			dist;
+	unsigned int	*color;
+}	t_pixel;
+
+typedef struct s_ray{
+	t_vec	dir;
+	double	dist;
+}	t_ray;
+
+typedef struct s_screen{
+	void	*mlx;
+	void	*win;
+	t_pixel	**pixel;
+	int		w;
+	int		h;
+	double	sin_unit;
+	double	cos_unit;
+	double	dist;
+	t_ray	*ray;
+	t_img	img;
+	t_vec	origin;
+	t_vec	dir;
+	t_vec	plane;
+}	t_screen;
 #endif
