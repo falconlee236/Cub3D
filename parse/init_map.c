6215f94957116ee26ced4511fb6adafdf1f9c882
yonyoo <yonyoo@student.42seoul.kr>
2024-03-15 22:05:42 +0900

    Update parse : Added Filename Checking

--- parse/init_map.c
+++ parse/init_map.c
@@ -6,7 +6,7 @@
 /*   By: yonyoo <yonyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/08 22:10:42 by yonyoo            #+#    #+#             */
-/*   Updated: 2024/03/15 21:24:03 by yonyoo           ###   ########seoul.kr  */
+/*   Updated: 2024/03/15 21:53:06 by yonyoo           ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,18 @@
 
 static int	check_filename(char *str)
 {
-	(void)str;
+	size_t	filename_len;
+
+	filename_len = ft_strlen(str);
+	if (ft_strncmp(str + filename_len - 4, ".cub", 4) != 0)
+		return (0);
 	return (1);
 }
 
 int	init_map(int argc, char **argv, t_map *map)
 {
-	if (argc != 2)
+	(void)map;
+	if (argc != 2 || !argv || !(argv[1]))
 		return (0);
 	if (!check_filename(argv[1]))
 		return (0);
--- parse/parse.h
+++ parse/parse.h
@@ -6,7 +6,7 @@
 /*   By: yonyoo <yonyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/05 20:13:23 by yonyoo            #+#    #+#             */
-/*   Updated: 2024/03/15 21:23:57 by yonyoo           ###   ########seoul.kr  */
+/*   Updated: 2024/03/15 22:05:18 by yonyoo           ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@
 # include <unistd.h>
 # include "../cub3d.h"
 # include "../libft/libft.h"
+
+typedef struct s_map	t_map;
 
 char	*get_next_line(int fd);
 char	*ft_strjoin_gnl(char **s1, char *s2);
