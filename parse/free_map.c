67782aecf844ae77b79770248c0b53f2e3dfd183
yonyoo <yonyoo@student.42seoul.kr>
2024-03-15 21:23:06 +0900

    Update Parse : Added free_map Function

--- parse/free_map.c
+++ parse/free_map.c
@@ -6,13 +6,30 @@
 /*   By: yonyoo <yonyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/15 21:19:09 by yonyoo            #+#    #+#             */
-/*   Updated: 2024/03/15 21:19:26 by yonyoo           ###   ########seoul.kr  */
+/*   Updated: 2024/03/15 21:22:53 by yonyoo           ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parse.h"
 
-void	free_map(t_map map)
+int	free_map(t_map *map)
 {
+	int	i;
+	int	j;
 
+	i = 0;
+	while (i < map->max_height)
+	{
+		j = 0;
+		while (j < map->max_width)
+		{
+			if ((map->map)[i][j])
+				free((map->map)[i][j]);
+			j++;
+		}
+		if ((map->map)[i])
+			free((map->map)[i]);
+		i++;
+	}
+	return (-1);
 }
