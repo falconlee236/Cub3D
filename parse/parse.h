934ac28c63eaadfacc34353ebca6035d3e285613
yonyoo <yonyoo@student.42seoul.kr>
2024-03-15 22:06:06 +0900

    Update parse : Fixed Header Issue

--- parse/parse.h
+++ parse/parse.h
@@ -6,12 +6,12 @@
 /*   By: yonyoo <yonyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/05 20:13:23 by yonyoo            #+#    #+#             */
-/*   Updated: 2024/03/15 22:05:18 by yonyoo           ###   ########seoul.kr  */
+/*   Updated: 2024/03/15 22:05:53 by yonyoo           ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef PARSE_H
+# define PARSE_H
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 42
 # endif
