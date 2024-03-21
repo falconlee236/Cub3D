6919993f1f2dc408e8c0891746fe759e980a7b14
yymin1022 <yymin1022@gmail.com>
2024-03-21 19:35:38 +0900

    Update Utils : Added exit_error Function

--- utils.c
+++ utils.c
@@ -6,11 +6,18 @@
 /*   By: yonyoo <yonyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/21 17:15:22 by yonyoo            #+#    #+#             */
-/*   Updated: 2024/03/21 17:35:12 by yonyoo           ###   ########seoul.kr  */
+/*   Updated: 2024/03/21 19:35:15 by yonyoo           ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+void	exit_error(char *str)
+{
+	ft_putstr_fd("Error\n", 2);
+	ft_putendl_fd(str, 2);
+	exit(-1);
+}
 
 void	free_str_arr(char **arr, char *str)
 {
@@ -36,14 +43,4 @@ int	ft_strcmp(const char *s1, const char *s2)
 	while (s1[i] && s2[i] && s1[i] == s2[i])
 		i++;
 	return (s1[i] - s2[i]);
-}
-
-int	get_arr_size(char **arr)
-{
-	int	i;
-
-	i = 0;
-	while (arr[i])
-		i++;
-	return (i);
 }
