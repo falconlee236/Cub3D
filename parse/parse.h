28135d0c572906b5b2fd9e3e54b3a03fbd90687c
yymin1022 <yymin1022@gmail.com>
2024-03-21 21:05:41 +0900

    Update parse : Fixed Header

--- parse/parse.h
+++ parse/parse.h
@@ -6,7 +6,7 @@
 /*   By: yonyoo <yonyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/05 20:13:23 by yonyoo            #+#    #+#             */
-/*   Updated: 2024/03/21 19:18:41 by yonyoo           ###   ########seoul.kr  */
+/*   Updated: 2024/03/21 21:04:53 by yonyoo           ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,12 +26,11 @@ typedef struct s_map	t_map;
 char	*get_next_line(int fd);
 char	*get_next_line_nonl(int fd);
 char	*ft_strjoin_gnl(char **s1, char *s2);
-int		alloc_map(t_map *map);
-int		free_map(t_map *map);
 int		get_color(char *rgb_str);
-int		set_map(char *filename, t_map *map);
+void	alloc_map(t_map *map);
 void	init_map(int argc, char **argv, t_map *map);
 void	read_file_data(char *filename, t_map *map);
+void	set_map(char *filename, t_map *map);
 void	*ft_free(char **target);
 
 #endif
